134247eae426b4b12c0bfd31336e797d5f6b3fae
intern2020 <phyozinmar1312000@gmail.com>
2020-10-06 19:56:19 +0630

    Added constant package

---
+++ TEACHERAPI/src/main/java/com/example/demo/constant/GlobalConstant.java
@@ -0,0 +1,14 @@
+package com.example.demo.constant;
+
+public class GlobalConstant {
+	public static final int SUCCESS = 1;
+	public static final int FAIL = 0 ;
+	
+	
+	public interface Message{
+		String SUCCESS_MESSAGE="Successful !!!";
+		String FAIL_MESSAGE = "Fail !!!";
+
+}
+}
+ 
