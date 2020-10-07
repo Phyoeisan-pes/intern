217a443b85b1ef0275c5d632dca5a3674449cd13
Your Name <you@example.com>
2020-10-07 08:37:52 -0700

    adding

---
+++ TEACHERAPI/src/main/java/com/example/demo/pojo/TeacherPojo.java
@@ -0,0 +1,28 @@
+package com.example.demo.pojo;
+
+public class TeacherPojo {
+
+	Long id;
+	String name;
+	String phone;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getname() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
+
